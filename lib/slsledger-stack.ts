9a68a303d02575ecb1f8427aec5f94738e70ad88
Slavo Vojacek <public@slavovojacek.com>
2023-03-19 16:26:48 +0000

    Working example

--- lib/slsledger-stack.ts
+++ lib/slsledger-stack.ts
@@ -16,6 +16,7 @@ export class ServerlessLedgerStack extends cdk.Stack {
     this.newDynamoTable();
     this.newLambdas();
     this.newRestApi(props.restApiStageName);
+    this.newWaf();
   }
 
   private newDynamoTable() {
@@ -34,6 +35,9 @@ export class ServerlessLedgerStack extends cdk.Stack {
         stageName,
         tracingEnabled: true,
         metricsEnabled: true,
+      },
+      defaultMethodOptions: {
+        apiKeyRequired: true, // API Key is required for all methods
       },
     });
 
@@ -65,6 +69,23 @@ export class ServerlessLedgerStack extends cdk.Stack {
           }),
         },
       });
+
+    const restApiUsagePlan = new cdk.aws_apigateway.UsagePlan(this, 'RestApiUsagePlan', {
+      name: 'Default',
+      throttle: {
+        rateLimit: 5,
+        burstLimit: 2,
+      },
+      quota: {
+        limit: 100,
+        period: cdk.aws_apigateway.Period.DAY,
+      },
+    });
+
+    restApiUsagePlan.addApiStage({ stage: this.restApi.deploymentStage });
+
+    const apiKey = this.restApi.addApiKey('TestApiKey');
+    restApiUsagePlan.addApiKey(apiKey);
   }
 
   private newLambdas() {
@@ -75,5 +96,70 @@ export class ServerlessLedgerStack extends cdk.Stack {
 
     this.dynamoTable.grantWriteData(this.lambdas.createAccountLambda);
     this.dynamoTable.grantWriteData(this.lambdas.transferFundsLambda);
+  }
+
+  private newWaf() {
+    const rules: Array<cdk.aws_wafv2.CfnWebACL.RuleProperty> = [
+      {
+        name: 'AWSManagedRulesCommonRuleSet',
+        priority: 1,
+        overrideAction: { none: {} },
+        statement: {
+          managedRuleGroupStatement: {
+            name: 'AWSManagedRulesCommonRuleSet',
+            vendorName: 'AWS',
+          },
+        },
+        visibilityConfig: {
+          cloudWatchMetricsEnabled: true,
+          metricName: 'AWSManagedRulesCommonRuleSet',
+          sampledRequestsEnabled: true,
+        },
+      },
+      {
+        name: 'IPRateLimit',
+        priority: 2,
+        action: {
+          block: {},
+        },
+        statement: {
+          rateBasedStatement: {
+            aggregateKeyType: 'IP',
+            limit: 1_000,
+          },
+        },
+        visibilityConfig: {
+          cloudWatchMetricsEnabled: true,
+          metricName: 'IPRateLimit',
+          sampledRequestsEnabled: true,
+        },
+      },
+    ];
+
+    const waf = new cdk.aws_wafv2.CfnWebACL(this, 'Waf', {
+      defaultAction: {
+        allow: {},
+      },
+      scope: 'REGIONAL',
+      visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        metricName: 'WebACL',
+        sampledRequestsEnabled: true,
+      },
+      rules,
+    });
+
+    const restApiArn = [
+      `arn:aws:apigateway:${this.region}::`,
+      'restapis',
+      this.restApi.restApiId,
+      'stages',
+      this.restApi.deploymentStage.stageName,
+    ].join('/');
+
+    new cdk.aws_wafv2.CfnWebACLAssociation(this, 'RestApiAssociation', {
+      webAclArn: waf.attrArn,
+      resourceArn: restApiArn,
+    });
   }
 }
